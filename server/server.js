9375b0077f589c7ff4a9fdc7ce6fc80c1f327062
Tayshaun M. Delos Santos <tayshaunds25@gmail.com>
2025-02-28 11:52:45 +0800

    Created the backend server for the client

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import records from './routes/record.js'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './config.env' })
+
+const PORT = process.env.PORT || 5050
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use('/record', records)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
